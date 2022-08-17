87842a33466238516a1925398adbf9f5b065507b
Hanseul Jun <hanseuljun@gmail.com>
2022-08-17 10:25:41 -0700

    Working on adding classes though there is a problem with FileVideoTrack.

---
+++ pyrgbd/build_librgbd.py
@@ -0,0 +1,49 @@
+from cffi import FFI
+from pathlib import Path
+import os
+
+
+def build_librgbd():
+    librgbd_include_dir = Path("../librgbd-binaries/1.3.0/arm64-mac/include").absolute()
+    librgbd_library_dir = Path("../librgbd-binaries/1.3.0/arm64-mac/bin").absolute()
+
+    ffi = FFI()
+
+    ffi.set_source('_librgbd',
+                   r'#include <rgbd/rgbd_capi.h>',
+                   include_dirs=[str(librgbd_include_dir)],
+                   libraries=['rgbd-1'],
+                   library_dirs=[str(librgbd_library_dir)],
+                   extra_link_args=[
+                       '-Wl,-rpath,/Users/hanseuljun/repos/pyrgbd/librgbd-binaries/1.3.0/arm64-mac/bin']
+                   )
+
+    cdef_lines = []
+    inside_cplusplus = False
+    with open(os.path.join(librgbd_include_dir, "rgbd/rgbd_capi.h")) as f:
+        lines = f.readlines()
+        for line in lines:
+            # Ignore lines only for when __cplusplus is defined.
+            if inside_cplusplus:
+                if line.startswith("#endif"):
+                    inside_cplusplus = False
+                else:
+                    continue
+            if line.startswith("#ifdef __cplusplus"):
+                inside_cplusplus = True
+            # Ignore the directives as cffi cannot handle them.
+            if line.startswith("#"):
+                continue
+            cdef_lines.append(line)
+
+    ffi.cdef("".join(cdef_lines))
+    ffi.compile()
+    print("built librgbd")
+
+
+def main():
+    build_librgbd()
+
+
+if __name__ == "__main__":
+    main()
--- pyrgbd/main.py
+++ pyrgbd/main.py
@@ -1,56 +1,82 @@
-from cffi import FFI
-from pathlib import Path
-import os
+from _librgbd import ffi, lib
 
+
+class FileInfo:
+    def __init__(self, ptr, owner):
+        print(f"FileInfo ptr: {ptr}")
+        self.ptr = ptr
+        self.owner = owner
+
+    def get_duration_us(self):
+        return lib.rgbd_file_info_get_duration_us(self.ptr)
+
+
+class FileVideoTrack:
+    def __init__(self, ptr, owner):
+        self.ptr = ptr
+        self.owner = owner
+
+    def get_track_number(self):
+        return lib.rgbd_file_video_track_get_track_number(self.ptr)
+
+    def get_width(self):
+        return lib.rgbd_file_video_track_get_width(self.ptr)
+
+    def get_height(self):
+        return lib.rgbd_file_video_track_get_height(self.ptr)
+
+
+class File:
+    def __init__(self, ptr):
+        print(f"File ptr: {ptr}")
+        self.ptr = ptr
+
+    def get_info(self):
+        return FileInfo(lib.rgbd_file_get_info(self.ptr), False)
+
+    def get_color_track(self):
+        color_track_ptr = lib.rgbd_file_tracks_get_color_track(self.ptr)
+        print(f"self.ptr: {self.ptr}")
+        print(f"color_track_ptr: {color_track_ptr}")
+        return FileVideoTrack(lib.rgbd_file_tracks_get_color_track(self.ptr), False)
+
+    def get_depth_track(self):
+        depth_track_ptr = lib.rgbd_file_tracks_get_depth_track(self.ptr)
+        print(f"self.ptr: {self.ptr}")
+        print(f"depth_track_ptr: {depth_track_ptr}")
+        return FileVideoTrack(depth_track_ptr, False)
+
+    def get_video_frame_count(self):
+        return lib.rgbd_file_get_video_frame_count(self.ptr)
+
+
+class FileParser:
+    def __init__(self, file_path):
+        self.ptr = lib.rgbd_file_parser_ctor_from_path(file_path.encode("utf8"))
+
+    def parse_no_frames(self):
+        print(f"FileParser self.ptr: {self.ptr}")
+        return File(lib.rgbd_file_parser_parse_no_frames(self.ptr))
+
+    def parse_all_frames(self):
+        print(f"FileParser self.ptr: {self.ptr}")
+        return File(lib.rgbd_file_parser_parse_all_frames(self.ptr))
 
 def main():
-    print("hey")
-    librgbd_include_dir = Path("../librgbd-binaries/1.3.0/arm64-mac/include").absolute()
-    librgbd_library_dir = Path("../librgbd-binaries/1.3.0/arm64-mac/bin").absolute()
-    print(f"librgbd_include_dir: {librgbd_include_dir}")
-    print(f"librgbd_library_dir: {librgbd_library_dir}")
-
-    ffibuilder = FFI()
-
-    ffibuilder.set_source('_librgbd',
-                          r'#include <rgbd/rgbd_capi.h>',
-                          include_dirs=[str(librgbd_include_dir)],
-                          libraries=['rgbd-1'],
-                          library_dirs=[str(librgbd_library_dir)],
-                          extra_link_args=[
-                              '-Wl,-rpath,/Users/hanseuljun/repos/pyrgbd/librgbd-binaries/1.3.0/arm64-mac/bin']
-                          )
-
-    cdef_lines = []
-    inside_cplusplus = False
-    with open(os.path.join(librgbd_include_dir, "rgbd/rgbd_capi.h")) as f:
-        lines = f.readlines()
-        for line in lines:
-            # Ignore lines only for when __cplusplus is defined.
-            if inside_cplusplus:
-                if line.startswith("#endif"):
-                    inside_cplusplus = False
-                else:
-                    continue
-            if line.startswith("#ifdef __cplusplus"):
-                inside_cplusplus = True
-            # Ignore the directives as cffi cannot handle them.
-            if line.startswith("#"):
-                continue
-            cdef_lines.append(line)
-
-    ffibuilder.cdef("".join(cdef_lines))
-    ffibuilder.compile()
-
-    from _librgbd import ffi, lib
-
     print(str(lib.RGBD_MAJOR_VERSION()))
     print(str(lib.RGBD_MINOR_VERSION()))
     print(str(lib.RGBD_PATCH_VERSION()))
 
-    video_path = "/Users/hanseuljun/repos/telegie-app/deps/librgbd/videos/joe.mkv"
-    file_parser_ptr = lib.rgbd_file_parser_ctor_from_path(video_path.encode("utf8"))
-
+    video_file_path = "/Users/hanseuljun/repos/telegie-app/deps/librgbd/videos/joe.mkv"
+    file_parser = FileParser(video_file_path)
+    file = file_parser.parse_all_frames()
+    print(f"get_video_frame_count: {file.get_video_frame_count()}")
+    file_info = file.get_info()
+    print(f"get_duration_us: {file_info.get_duration_us()}")
+    depth_track = file.get_depth_track()
+    print(f"get_track_number: {depth_track.get_track_number()}")
+    print(f"color width: {depth_track.get_width()}")
+    print(f"color height: {depth_track.get_height()}")
 
 
 if __name__ == "__main__":
